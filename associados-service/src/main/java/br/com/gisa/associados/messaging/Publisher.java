af518de8a3d67ad1de7aec7cddd7bbe2b0bb22f8
Luis Jhonne <luis.jhonne@gmail.com>
2022-04-10 23:54:22 -0300

    Ajuste na publicação da mensagem

--- associados-service/src/main/java/br/com/gisa/associados/messaging/Publisher.java
+++ associados-service/src/main/java/br/com/gisa/associados/messaging/Publisher.java
@@ -1,21 +1,28 @@
 package br.com.gisa.associados.messaging;
 
+import br.com.gisa.associados.model.AutorizacaoExameConsulta;
+import br.com.gisa.associados.repository.AutorizacaoRepository;
+import com.google.gson.Gson;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class Publisher {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    //@Autowired
+    private final AmqpTemplate queueSender;
 
     @Autowired
-    private Queue queue;
+    public Publisher(AmqpTemplate queueSender){
+        this.queueSender = queueSender;
+    }
 
-    public void send(String order) {
-        rabbitTemplate.convertAndSend(this.queue.getName(), order);
+    public void publishInQueue(AutorizacaoExameConsulta autorizacaoExameConsulta){
+        Gson gson = new Gson();
+        queueSender.convertAndSend("gisa-exchange", "gisa-autorizacao-solicitacao",  gson.toJson(autorizacaoExameConsulta));
     }
 
 }
--- associados-service/src/main/java/br/com/gisa/associados/service/impl/AutorizacaoServiceImpl.java
+++ associados-service/src/main/java/br/com/gisa/associados/service/impl/AutorizacaoServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.gisa.associados.service.impl;
 
 import br.com.gisa.associados.enums.SituacaoAutorizacaoEnum;
 import br.com.gisa.associados.exceptions.NotFoundException;
+import br.com.gisa.associados.messaging.Publisher;
 import br.com.gisa.associados.model.Associado;
 import br.com.gisa.associados.model.AutorizacaoExameConsulta;
 import br.com.gisa.associados.repository.AutorizacaoRepository;
@@ -22,12 +23,12 @@ public class AutorizacaoServiceImpl implements AutorizacaoService {
 
     private AutorizacaoRepository autorizacaoRepository;
 
-    private final AmqpTemplate queueSender;
+    private Publisher publisher;
 
     @Autowired
-    public AutorizacaoServiceImpl(AutorizacaoRepository autorizacaoRepository, AmqpTemplate queueSender){
+    public AutorizacaoServiceImpl(AutorizacaoRepository autorizacaoRepository, Publisher publisher){
         this.autorizacaoRepository = autorizacaoRepository;
-        this.queueSender = queueSender;
+        this.publisher = publisher;
     }
 
     public AutorizacaoExameConsulta save(AutorizacaoExameConsulta associado){
@@ -39,10 +40,10 @@ public class AutorizacaoServiceImpl implements AutorizacaoService {
     public AutorizacaoExameConsulta autorizarExameConsulta(AutorizacaoExameConsulta autorizacaoExameConsulta) {
 
         autorizacaoExameConsulta.setSituacao(SituacaoAutorizacaoEnum.SOLICITADO);
-        autorizacaoExameConsulta.setDataAutorizacao(new Date());
+        autorizacaoExameConsulta.setDataSolicitacao(new Date());
         autorizacaoExameConsulta.setCodigoAutorizacao(UUID.randomUUID().toString());
 
-        publishInQueue(autorizacaoExameConsulta);
+        publisher.publishInQueue(autorizacaoExameConsulta);
 
         return save(autorizacaoExameConsulta);
     }
@@ -51,10 +52,5 @@ public class AutorizacaoServiceImpl implements AutorizacaoService {
     public AutorizacaoExameConsulta consultarExameConsulta(String codigoAutorizacao) throws NotFoundException {
         return autorizacaoRepository.findOneByCodigoAutorizacao(codigoAutorizacao)
                 .orElseThrow(() -> new NotFoundException("Autorização não encontrada"));
-    }
-
-    public void publishInQueue(AutorizacaoExameConsulta autorizacaoExameConsulta){
-        Gson gson = new Gson();
-        queueSender.convertAndSend("gisa-exchange", "gisa-autorizacao-solicitacao",  gson.toJson(autorizacaoExameConsulta));
     }
 }
